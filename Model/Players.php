be11f116bb6431e619868bd2d326a9ca95267fd7
ivoli <ivol1mb3rg@gmail.com>
2023-10-25 11:40:05 +0200

    Added getters and Setters to model

--- Model/Players.php
+++ Model/Players.php
@@ -2,5 +2,56 @@
 
 class Players
 {
+private $name;
+private $age;
 
+private $position;
+
+
+    /**
+     * @return mixed
+    */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+    * @param mixed $name
+    */
+public function setName($name)
+{
+    $this->name = $name;
+}
+
+    /**
+    * @return mixed
+    */
+    public function getAge()
+    {
+        return $this->age;
+    }
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param mixed $position
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
 }
